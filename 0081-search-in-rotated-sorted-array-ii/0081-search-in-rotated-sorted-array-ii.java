cac10a534581cfaaba4507d02849602624fecc8d
RahilShaik21 <rahilshaik9090@gmail.com>
2026-09-09 18:55:47 +0530

    Time: 0 ms (100%), Space: 44.9 MB (81.86%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean search(int[] nums, int k) {
+         int s=0,e=nums.length-1;
+      while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==k) return true;
+            if(nums[mid]==nums[s]&&nums[mid]==nums[e]){
+                s++;
+                e--;
+                continue;
+            }
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=k&&nums[mid]>k){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[e]>=k&&nums[mid]<k){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }
+
+        }
+       return false;
+    }
+}
