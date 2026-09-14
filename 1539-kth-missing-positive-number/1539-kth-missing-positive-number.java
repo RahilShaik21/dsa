4ee1905594e52fab1de47dd398d24cc0d4ee16df
RahilShaik21 <rahilshaik9090@gmail.com>
2026-09-14 12:24:04 +0530

    Time: 0 ms (100%), Space: 44.5 MB (57.36%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
-             int missingVal=0;
-        int start=0;
-        int end=arr.length-1;
-        //here we are finding that where missing values are greater than the give k
-        while (start<=end){
-            int mid=start+(end-start)/2;
-            missingVal=arr[mid]-(mid+1);//here we finding the mising values that exists
-            // by subtarcting the actual value in the arr - the value that should exists there ie.index+1
-            if(missingVal>=k){
-                end=mid-1;
+    public int findKthPositive(int[] nums, int k) {
+         int s=0,e=nums.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            int missing=nums[mid]-(mid+1);
+            if(missing<k){
+                s=mid+1;
             }
             else{
-                start=mid+1;
+                e=mid-1;
             }
         }
-        return start+k;
+        return k+e+1;  
     }
-    }
+}
